aea99f70f5ac05a8232b7bb5527607f579d6d289
Derek Yu <derekyu624@hotmail.com>
2018-10-06 15:53:22 -0400

    Fix merge conflicts

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, Response
+from flask_socketio import SocketIO, emit, send
 from users import createUser
 import json
 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'VdC8xBfJ4u66hycU'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    socketio.emit('Hello')
+    return 'Hello, World!!'
 
 @app.route('/users', methods=['POST'])
 def newUser():
@@ -20,3 +25,15 @@ def newUser():
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
     else:
         return json.dumps({'success':False}), 400, {'ContentType':'application/json'} 
+
+@socketio.on('connect')
+def handleConnect():
+    emit('Connected to backend!')
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message:' + msg)
+    send(msg)
+
+if __name__ == '__main__':
+    socketio.run(app)

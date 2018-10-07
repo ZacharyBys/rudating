97258615a129c54bc76dfa1a34f6c5caf23c3ed4
zacharybys <zachary.bys@gmail.com>
2018-10-07 04:20:11 -0400

    Added get phone numbers endpoint

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, Response
 from flask_socketio import SocketIO, emit, send, join_room
 from flask_cors import CORS
 from flask_uploads import UploadSet, configure_uploads, IMAGES
-from users import createUser, getUser, activateUser, userIsInChat, addNewNumber
+from users import createUser, getUser, activateUser, userIsInChat, addNewNumber, getNumbers
 from profilepic import upload_picture
 from matchingThread import MatchingThread
 import json, time
@@ -48,6 +48,16 @@ def addNumber():
 
     return json.dumps({'success':False}), 400, {'ContentType':'application/json'}   
 
+@app.route('/phone', methods=['GET'])
+def getNum():
+    if 'id' in request.args:
+        id = request.args.get('id')
+        result = getNumbers(int(id))
+
+        if result != -1:
+            return json.dumps({'numbers':result}), 200, {'ContentType':'application/json'}
+
+    return json.dumps({'numbers':[]}), 400, {'ContentType':'application/json'}   
 
 @app.route('/user', methods=['GET'])
 def retrieveUser():
--- backend/users.py
+++ backend/users.py
@@ -22,6 +22,23 @@ def addNewNumber(userId, num):
     else:
         return -1
 
+def getNumbers(userId):
+    userId = userId if userId != None else 0
+
+    key = client.key('Users', userId)
+    entity = client.get(key)
+
+    if 'savedNumbers' in entity and entity['savedNumbers'] != None:
+        nums = entity['savedNumbers']
+
+    resultId = client.get(key).id
+
+    if resultId == key.id:
+        return nums
+    else:
+        return -1
+
+
 def createUser(firstName, lastName, gender, number):
 
     key = client.key('Users', random.randint(1, 99999999))

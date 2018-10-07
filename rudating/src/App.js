91e359427c5ab57e5af5d034b4f9b531b9bcf186
zacharybys <zachary.bys@gmail.com>
2018-10-07 08:28:33 -0400

    Added list of people users has matched with

--- rudating/src/App.js
+++ rudating/src/App.js
@@ -6,6 +6,7 @@ import MakeProfile from './pages/MakeProfile';
 import Login from './pages/Login';
 import Lobby from './pages/Lobby';
 import InterestPage from './pages/InterestPage';
+import NumbersPage from './pages/NumbersPage';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css'
@@ -20,6 +21,7 @@ class App extends Component {
 						<Route path="/interest" component={InterestPage}/>
 						<Route path="/lobby" component={Lobby}/>
                         <Route path="/login" component={Login}/>
+						<Route path="/numbers" component={NumbersPage}/>
 					</Switch>
 				</BrowserRouter>
 			)
